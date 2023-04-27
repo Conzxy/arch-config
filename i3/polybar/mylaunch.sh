22f8761273575a6d95fb0ac0b2ea0743dac3739a
Conzxy <1967933844@qq.com>
2023-04-28 00:48:30 +0800

    feat(sync-config): Add new dotfiles

---
+++ i3/polybar/mylaunch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+if type 'xrandr'; then
+  for m in $(xrandr --query | grep ' connected' | cut -d" " -f1); do
+    POLYBAR_MONITOR=$m polybar --reload example 1>/dev/null 2>&1 | tee -a /tmp/polybar2.log & disown
+  done
+else
+  polybar -c ~/.config/polybar/config.ini example 1>/dev/null 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
+
+
+echo "Bars launched..."
---
+++ software/todoman/config.py
@@ -0,0 +1,6 @@
+path = "~/.local/share/calendars/*"
+date_format = "%Y-%m-%d"
+time_format = "%H:%M"
+default_list = "Personal"
+# default_due = 48
+# default_command = list --startable
